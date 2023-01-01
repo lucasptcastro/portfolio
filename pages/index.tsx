9fb607300065478ee7098d58c2da5e3c0e55060f
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-01 15:12:52 -0300

    🦄 refactor: remove comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,4 @@
-import { Inter } from "@next/font/google";
 import Portfolio from "../src/index";
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// export default function Home() {
-//   return (
-//     <>
-//       <Portfolio />
-//     </>
-//   );
-// }
 
 const Index: React.FC = () => {
   return (
