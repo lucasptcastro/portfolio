dfb38839ff162e6e2befb7077330887712710a7a
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-01-02 21:25:07 -0300

    🦄 refactor: refactor navbar

--- src/index.tsx
+++ src/index.tsx
@@ -10,93 +10,17 @@ import {
   Row,
   Col,
 } from "antd";
-import type { MenuProps } from "antd";
 import React from "react";
-import Image from "next/image";
-import NodeJSLogo from "../public/nodejs.svg";
+import Navbar from "../components/_ui/Navbar/navbar";
 
 const { Header, Footer, Sider, Content } = Layout;
 
-const items: any = [
-  {
-    key: "1",
-    label: (
-      <Typography.Text className="font-concert-one text-xl hover:text-yellow-600 hover:cursor-pointer">
-        Início
-      </Typography.Text>
-    ),
-  },
-  {
-    key: "2",
-    label: (
-      <Typography.Text className="font-concert-one text-xl hover:text-yellow-600 hover:cursor-pointer">
-        Sobre
-      </Typography.Text>
-    ),
-  },
-  {
-    key: "3",
-    label: (
-      <Typography.Text className="font-concert-one text-xl hover:text-yellow-600 hover:cursor-pointer">
-        Habilidades
-      </Typography.Text>
-    ),
-  },
-  {
-    key: "4",
-    label: (
-      <Typography.Text className="font-concert-one text-xl hover:text-yellow-600 hover:cursor-pointer">
-        Educação
-      </Typography.Text>
-    ),
-  },
-  {
-    key: "5",
-    label: (
-      <Typography.Text className="font-concert-one text-xl hover:text-yellow-600 hover:cursor-pointer">
-        Experiências
-      </Typography.Text>
-    ),
-  },
-  {
-    key: "6",
-    label: (
-      <Typography.Text className="font-concert-one text-xl hover:text-yellow-600 hover:cursor-pointer">
-        Contatos
-      </Typography.Text>
-    ),
-  },
-];
-
 const Portfolio: React.FC = () => {
-  const [current, setCurrent] = React.useState("1");
-
-  const onClick: MenuProps["onClick"] = (e) => {
-    console.log("click ", e);
-    setCurrent(e.key);
-  };
-
   return (
     <>
-      <Layout className="layout">
-        <Affix>
-          <Row className="justify-around items-center bg-[#fff] rounded-sm">
-            <Col>
-              <Image
-                src={NodeJSLogo}
-                alt="NodeJS Logo"
-                width={40}
-                height={24}
-              />
-            </Col>
-            <Col>
-              <Typography>
-                <Space size={40}>{items.map((item: any) => item.label)}</Space>
-              </Typography>
-            </Col>
-          </Row>
-        </Affix>
-        <Content className="bg-[#F8F8F8] h-screen">
+      <Layout className="h-screen">
+        <Navbar />
+        <Content className="bg-[#F8F8F8] ">
           <div className="p-32">
             <Row className="flex flex-row justify-around items-center">
               <Col>
